b242537118f649ecaee18db96add79cd085fa42c
Seon-dongun <77486690+Seon-dongun@users.noreply.github.com>
2023-10-16 22:07:13 +0900

    Update ABPlayerController.cpp

--- ArenaBattle/Player/ABPlayerController.cpp
+++ ArenaBattle/Player/ABPlayerController.cpp
@@ -47,19 +47,16 @@ void AABPlayerController::BeginPlay()
 	FInputModeGameOnly GameOnlyInputMode;
 	SetInputMode(GameOnlyInputMode);
 
-	SaveGameInstance = Cast<UABSaveGame>(UGameplayStatics::LoadGameFromSlot(TEXT("Player0"),0)); 
 	// 세이브 파일을 불러오는 LoadGameFromSlot 함수. TEXT 타입의 저장 파일 이름과 대응되는 
 	// 플레이어의 아이디를 인자로 주면 된다. 싱글 플레이의 경우 항상 플레이어 아이디는 0이다
-	if (SaveGameInstance)
-	{
-		SaveGameInstance->RetryCount++; // 로드 성공 시, 기존의 RetryCount를 증가
-	}
-	else
+	SaveGameInstance = Cast<UABSaveGame>(UGameplayStatics::LoadGameFromSlot(TEXT("Player0"),0)); 
+	
+	if (!SaveGameInstance)
 	{
 		SaveGameInstance = NewObject<UABSaveGame>(); // 로드 실패 시, 저장된 값이 없기 때문에 새로운 세이브 객체를 생성
 		SaveGameInstance->RetryCount = 0; // 처음 세이브 생성 시 RetryCount는 0으로 초기화됨
 	}
-
+	
+	SaveGameInstance->RetryCount++; // 로드 성공 시, 기존의 RetryCount를 증가
 	K2_OnGameRetryCount(SaveGameInstance->RetryCount);
-
 }
